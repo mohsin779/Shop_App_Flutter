b40cb3dcb6b0186347a35effe5a1c64a9638acc0
mohsin779 <mianmohsin779@gmail.com>
2023-01-19 16:13:10 +0500

    make cart item dismissable and write a function to remove this dismissable item from cart in provider cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -60,6 +60,7 @@ class CartScreen extends StatelessWidget {
                 itemCount: cart.items.length,
                 itemBuilder: (ctx, i) => CartItem(
                     cart.items.values.toList()[i].id,
+                    cart.items.keys.toList()[i],
                     cart.items.values.toList()[i].title,
                     cart.items.values.toList()[i].quantity,
                     cart.items.values.toList()[i].price)))
