4a2d531d42b22ea8b656ec0d017df5b1e353b18a
mohsin779 <mianmohsin779@gmail.com>
2023-01-20 10:22:31 +0500

    show a dialog box in dismissable cart item widget

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -33,6 +33,30 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text("Are you sure?"),
+                  content: Text(
+                    'Do you want to remove item from the cart?',
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('No'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop(false);
+                      },
+                    ),
+                    TextButton(
+                      child: Text('Yes'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop(true);
+                      },
+                    )
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
