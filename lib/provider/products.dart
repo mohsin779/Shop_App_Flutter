531c34c278593e4aa9f669a0508a265b1d73e965
mohsin779 <mianmohsin779@gmail.com>
2023-01-23 10:06:58 +0500

    setup firebase for database and send a simple post request to save newly added product into database

--- lib/provider/products.dart
+++ lib/provider/products.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
 
 import './product.dart';
 
@@ -65,6 +67,18 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    final url = Uri.parse(
+        'https://shopping-app-flutter-e95b5-default-rtdb.firebaseio.com/products.json');
+    http.post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        "imageUrl": product.imageUrl,
+        'price': product.price,
+        "isFavorite": product.isFavorite,
+      }),
+    );
     final newProduct = Product(
       title: product.title,
       description: product.description,
