328d7675ac961efbfd54b07e4d83b65f020777f8
mohsin779 <mianmohsin779@gmail.com>
2023-01-24 15:04:09 +0500

    added a auth_screen and make it home route

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import './provider/orders.dart';
 import "./screens/orders_screen.dart";
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,7 +40,7 @@ class MyApp extends StatelessWidget {
                 secondary: Colors.deepOrange,
               ),
         ),
-        home: ProductsOverViewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
